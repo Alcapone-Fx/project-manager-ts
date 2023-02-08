afcf47e3fbbfd55f9b543f508cd472156fdf2019
Felix Navas <navasfx19@gmail.com>
2023-02-07 22:29:14 -0600

    ✨ Attach form in host HTML element

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,21 @@
-// TS code here
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  HTMLElement: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+    
+    const importedHTMLNode = document.importNode(this.templateElement.content, true);
+    this.HTMLElement = importedHTMLNode.firstElementChild as HTMLFormElement;
+
+    this.attachNode();
+  }
+
+  private attachNode() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.HTMLElement);
+  }
+}
+
+const projectInput = new ProjectInput();
