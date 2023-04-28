166af847ab22ec548f55af17c734cd5e362113fc
Felix Navas <navasfx19@gmail.com>
2023-04-27 21:47:49 -0600

    ♻️ Create class for ProjectItem

--- src/app.ts
+++ src/app.ts
@@ -149,6 +149,33 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
   abstract renderContent(): void;
 }
 
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+  private project: Project;
+
+  get persons() {
+    return this.project.people === 1
+      ? "1 person"
+      : `${this.project.people} persons`;
+  }
+
+  constructor(hostId: string, project: Project) {
+    super("single-project", hostId, false, project.id);
+    this.project = project;
+
+    this.configure();
+    this.renderContent();
+  }
+
+  configure() {}
+  renderContent() {
+    this.HTMLElement.querySelector("h2")!.textContent = this.project.title;
+    this.HTMLElement.querySelector(
+      "h3"
+    )!.textContent = `${this.persons} assgined`;
+    this.HTMLElement.querySelector("p")!.textContent = this.project.description;
+  }
+}
+
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
   assignedProjects: Project[];
 
@@ -186,9 +213,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     listElement!.innerHTML = "";
 
     for (const project of this.assignedProjects) {
-      const listItem = document.createElement("li");
-      listItem.textContent = project.title;
-      listElement?.appendChild(listItem);
+      new ProjectItem(this.HTMLElement.querySelector("ul")!.id, project);
     }
   }
 }
